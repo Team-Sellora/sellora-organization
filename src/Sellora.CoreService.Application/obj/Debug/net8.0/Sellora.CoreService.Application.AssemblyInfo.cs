a3c34ca586c6be12f3b8ea01d404e144807e5ddc
Gayuth-W <gayuthdemika@gmail.com>
2026-08-26 12:16:45 +0530

    CSP-64 feat: add IProvinceReadService interface

--- src/Sellora.CoreService.Application/obj/Debug/net8.0/Sellora.CoreService.Application.AssemblyInfo.cs
+++ src/Sellora.CoreService.Application/obj/Debug/net8.0/Sellora.CoreService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sellora.CoreService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd5ccbe6c097092cd24d895169edd1286f6e1ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b1a6033b4221a1e01729eee6e340a35cc053a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sellora.CoreService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sellora.CoreService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
