3620562249ba31a49da50132b928f627eb5b2f58
Gayuth-W <gayuthdemika@gmail.com>
2026-08-29 00:07:29 +0530

    CSP-97 feat: expose company admin list endpoint

--- src/Sellora.CoreService.Api/Program.cs
+++ src/Sellora.CoreService.Api/Program.cs
@@ -17,6 +17,8 @@ using Sellora.CoreService.Application.Provinces;
 using Sellora.CoreService.Infrastructure.Provinces;
 using Sellora.CoreService.Application.AreaManagers;
 using Sellora.CoreService.Infrastructure.AreaManagers;
+using Sellora.CoreService.Application.CompanyAdmins;
+using Sellora.CoreService.Infrastructure.CompanyAdmins;
 using Sellora.CoreService.Application.Shops;
 using Sellora.CoreService.Infrastructure.Shops;
 using Sellora.CoreService.Application.Agencies;
@@ -161,6 +163,8 @@ builder.Services.AddScoped<IProvinceReadService, ProvinceReadService>();
 builder.Services.AddScoped<IHierarchyDeactivationService, HierarchyDeactivationService>();
 
 builder.Services.AddScoped<IAreaManagerReadService, AreaManagerReadService>();
+
+builder.Services.AddScoped<ICompanyAdminReadService, CompanyAdminReadService>();
 
 builder.Services.AddScoped<IShopRegistrationService, ShopRegistrationService>();
 
